23518c105a180c805688417e2c24f9d2fd4289f6
brayn <voidpointers@hotmail.com>
2020-03-23 00:42:56 +0800

    fix

--- app/Etsy/Requests/ListingRequest.php
+++ app/Etsy/Requests/ListingRequest.php
@@ -29,7 +29,7 @@ class ListingRequest
                         'listing_id' => $datum['listing_id']
                     ]
                 ]);
-                $inventory[$key] = $temp;
+                $inventory[$key] = $temp['results']['products'];
             }
             dd($inventory);
 
