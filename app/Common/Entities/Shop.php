6f30cb9efe8fc93ada51d83985595b24a89adfa9
brayn <voidpointers@hotmail.com>
2020-03-23 17:37:02 +0800

    增加店铺关联关系

--- app/Common/Entities/Shop.php
+++ app/Common/Entities/Shop.php
@@ -6,5 +6,12 @@ use App\Model;
 
 class Shop extends Model
 {
+    protected $hidden = ['access_token', 'access_secret'];
 
+    protected $table = 'shops';
+
+    public function declare()
+    {
+        return $this->hasOne(Declares::class, 'shop_id', 'shop_id');
+    }
 }
