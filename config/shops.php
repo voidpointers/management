50bf38b980e63d19fd950eab3cb9621ac21db753
brayn <voidpointers@hotmail.com>
2020-03-26 10:15:22 +0800

    增加店铺配置

---
+++ config/shops.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    16333181 => [
+        'shop_name' => 'FastestSloth',
+        'spider' => 'http://149.248.7.236:6800',
+        'user_id' => 125136382
+    ],
+    16407439 => [
+        'shop_name' => 'CrystalMaggie',
+        'spider' => 'http://185.207.155.17:6800',
+        'user_id' => 126311338
+    ]
+];
