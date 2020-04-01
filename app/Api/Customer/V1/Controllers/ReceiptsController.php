1d2518b13ca11216ce08fb6517b78124e92f4517
brayn <voidpointers@hotmail.com>
2020-04-01 14:33:43 +0800

    修复消息关联订单

--- app/Api/Customer/V1/Controllers/ReceiptsController.php
+++ app/Api/Customer/V1/Controllers/ReceiptsController.php
@@ -5,17 +5,9 @@ namespace Api\Customer\V1\Controllers;
 use Api\Order\V1\Transforms\ReceiptTransformer;
 use App\Controller;
 use Customer\Entities\Message;
-use Order\Services\ReceiptService;
 
 class ReceiptsController extends Controller
 {
-    protected $receiptService;
-
-    public function __construct(ReceiptService $receiptService)
-    {
-        $this->receiptService = $receiptService;
-    }
-
     public function show($convo_id)
     {
         $message = Message::where(['conversation_id' => $convo_id])->first();
