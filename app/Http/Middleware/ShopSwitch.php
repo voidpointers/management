f1ce5544e07b82a3bbeab4396a08b3874ef5e5fa
brayn <voidpointers@hotmail.com>
2020-04-02 15:49:26 +0800

    去除header头shop-id

--- app/Http/Middleware/ShopSwitch.php
+++ app/Http/Middleware/ShopSwitch.php
@@ -17,9 +17,6 @@ class ShopSwitch
     public function handle($request, Closure $next)
     {
         $shop_id = $request->input('shop_id', 0);
-        if (1 > $shop_id && -1 != $shop_id) {
-            $shop_id = $request->header('shop_id');
-        }
         Cache::store('array')->put('shop_id', $shop_id);
         return $next($request);
     }
