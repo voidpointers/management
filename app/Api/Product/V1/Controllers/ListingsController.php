e04d75121c38d4bc95c7ff741789e7df02128601
brayn <voidpointers@hotmail.com>
2020-05-03 16:22:19 +0800

    处理店铺ID

--- app/Api/Product/V1/Controllers/ListingsController.php
+++ app/Api/Product/V1/Controllers/ListingsController.php
@@ -44,7 +44,7 @@ class ListingsController extends Controller
 
     public function pull(Request $request)
     {
-        $data = $this->listingRequest->pull(['shop_id' => shop_id()]);
+        $data = $this->listingRequest->pull(['shop_id' => $request->input('shop_id', 0)]);
 
         return $this->response->array(['msg' => 'success']);
     }
